9eca7d3c3ac0a789ef988549609e3eb49cd63aa3
Eduard Bardají Puig <ebardaji@gmail.com>
2017-08-22 19:49:10 +0200

    Adds HMR

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,31 @@
 const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
+  devServer: {
+    hot: true,
+    port: 9000,
+    publicPath: '/',
+  },
   entry: './src/index.ts',
   output: {
     filename: 'bundle.js',
     path: path.resolve('dist'),
+    publicPath: '/',
   },
   module: {
     rules: [
-      {test: /\.tsx?/, use: ['babel-loader', 'ts-loader']},
+      {test: /\.tsx?/, exclude: /node_modules/, use: ['babel-loader', 'ts-loader']},
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
   resolve: {
     extensions: ['.ts', '.tsx', '.js', '.json'],
-    mainFields: ['types', 'main'],
-  }
+    mainFields: ['browser', 'module', 'main', 'types'],
+  },
+  watch: true,
 }
